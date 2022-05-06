730d48069c36ef3ba5d63f9c3bd4f33e35a9d43d
Akinelo <akinelo571@gmail.com>
2022-05-06 09:13:42 +0200

    Initial mobile version commit

---
+++ LORHAPI-ClientMobile/LORHAPI-ClientMobile/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LORHAPI_ClientMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
