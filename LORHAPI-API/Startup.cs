bdb2618096b9a77ff8c7630664793367eebff882
Akinelo <akinelo571@gmail.com>
2022-05-10 08:44:41 +0200

    Model Insertion-Users, Controller Users-Insertions

--- LORHAPI-API/Startup.cs
+++ LORHAPI-API/Startup.cs
@@ -1,7 +1,9 @@
+using LORHAPI_API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +30,8 @@ namespace LORHAPI_API
         {
 
             services.AddControllers();
+            services.AddDbContext<Db_Context>(options => //ajout pour accès BDD
+            options.UseMySQL(Configuration.GetConnectionString("DefaultConnection")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "LORHAPI_API", Version = "v1" });
