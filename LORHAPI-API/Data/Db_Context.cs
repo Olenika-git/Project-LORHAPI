2faeddd0052244ff2d85749b866521c6441f8c86
gillesgiac <gilles.giac@gmail.com>
2022-05-11 09:36:08 +0200

    MAJ

--- LORHAPI-API/Data/Db_Context.cs
+++ LORHAPI-API/Data/Db_Context.cs
@@ -15,20 +15,11 @@ namespace LORHAPI_API.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<Insertion> Insertions { get; set; }
-<<<<<<< HEAD
-=======
-        public DbSet<Organization> Organizations { get; set; }
-
->>>>>>> 3d2e19630eaac164107ead4f5cf5abae72199636
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().ToTable("Users");
             modelBuilder.Entity<Insertion>().ToTable("Insertion");
-<<<<<<< HEAD
-=======
-            modelBuilder.Entity<Organization>().ToTable("Organization");
->>>>>>> 3d2e19630eaac164107ead4f5cf5abae72199636
         }
     }
 }
