44686fda6e2662e0f05f9db380425ea274e50d34
Akinelo <akinelo571@gmail.com>
2022-06-13 22:09:43 +0200

    Removed NouiSlider
    no more needed

--- LORHAPI-Client/wwwroot/js/site.js
+++ LORHAPI-Client/wwwroot/js/site.js
@@ -1,10 +1,1 @@
-﻿var slider = document.getElementById('slider');
-
-noUiSlider.create(slider, {
-    start: [15, 100],
-    connect: true,
-    range: {
-        'min': 15,
-        'max': 100
-    }
-});
+﻿
